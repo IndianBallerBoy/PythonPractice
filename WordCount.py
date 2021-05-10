b01a1a6eca9d3e6313ff52c3e6defc864731dfcf
IndianBallerBoy <josungchuljojo@gmail.com>
2021-05-10 11:04:17 -0700

    Create WordCount.py

---
+++ WordCount.py
@@ -0,0 +1,10 @@
+
+
+print("What's on your mind today?")
+
+UserMind = input()
+
+theWords = str(len(UserMind.split()))
+
+print("Nice! You told me what is on your mind in " + theWords + ' words')
+
